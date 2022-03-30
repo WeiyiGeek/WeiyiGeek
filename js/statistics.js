82aa31c7b61e840d15960eec55667732acd15283
WeiyiGeek <master@weiyigeek.top>
2022-03-30 18:02:16 +0800

    baidu

---
+++ js/statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8bb888f4c802ff4cd2fdbc10d8ab7069";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
