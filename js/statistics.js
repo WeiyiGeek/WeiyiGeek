cae95fd0990e0ebeac7488cac91e301b62585f2e
WeiyiGeek <master@weiyigeek.top>
2022-04-04 12:57:01 +0800

    shema eng

--- js/statistics.js
+++ js/statistics.js
@@ -1,7 +1,15 @@
 var _hmt = _hmt || [];
 (function() {
+  // 百度统计提交
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?8bb888f4c802ff4cd2fdbc10d8ab7069";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
-})();
+
+  // 头条统计提交
+  var el = document.createElement("script");
+  el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?18bdfa6918177f056dfc80b20fb9315864c2065d5090edbac9a4751ea95d7d0565e0a2ada1d5e86b11e7de7c1a83287d04743a02fd1ee8dd8558a8cad50e91cb354f8c6f3f78e5fd97613c481f678e6d";
+  el.id = "ttzz";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(el, s);
+})(window);
